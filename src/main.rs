4a03bfa1231deb8365ba8de0b8749c6d50fb825f
Niklas Larsson <niklaslarsson95@gmail.com>
2020-07-05 12:16:20 +1000

    Ugly sine wave

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,51 @@ use console_engine::KeyCode;
 
 fn main() {
     // Initialise screen 20x20 characters, 10 fps
-    let mut engine = console_engine::ConsoleEngine::init(20,20,10);
+    const WIDTH: u32 = 100;
+    const HEIGHT: u32 = 50;
 
-    let mut frame = 0;
-    let mut offset = 0;
+    const WIDTHI: i32 = (WIDTH as i32) -1;
+    const HEIGHTI: i32 = (HEIGHT as i32) -1;
 
+    let mut engine = console_engine::ConsoleEngine::init(WIDTH,HEIGHT,30);
+
+    let mut t: f32 = 0.0;
+    let inc = 0.1;
+
+    let mut xs = vec![];
+    let mut ys = vec![];
     loop {
         // Wait for next frame + capture input
         engine.wait_frame();
 
         engine.clear_screen();
 
-        // Draw a diagonal line which scans across the screen
-        engine.line(0 + offset,0,19 + offset,19,pixel::pxl('#'));
+        //Border Lines
+        engine.line(0, 0, WIDTHI, 0, pixel::pxl('-'));
+        engine.line(0, HEIGHTI, WIDTHI, HEIGHTI, pixel::pxl('-'));
+        engine.line(0, 0, 0, HEIGHTI, pixel::pxl('|'));
+        engine.line(WIDTHI, 0, WIDTHI, HEIGHTI, pixel::pxl('|'));
+
+        //Corners
+        engine.set_pxl(0,0, pixel::pxl('+'));
+        engine.set_pxl(0,HEIGHTI, pixel::pxl('+'));
+        engine.set_pxl(WIDTHI,0, pixel::pxl('+'));
+        engine.set_pxl(WIDTHI,HEIGHTI, pixel::pxl('+'));
+
+        // Add a new point 
+        let t_i = (t * 5.0).floor() as i32;
+        let s_i = (t.sin() * 15.0).floor() as i32;
+        let x = t_i;
+        let y = (HEIGHTI / 2) + s_i;
+        xs.push(x);
+        ys.push(y);
+
+        for i in 0..xs.len() {
+            engine.set_pxl(xs[i],ys[i],pixel::pxl('x'));
+        }
+
+        // PRint t
+        engine.print(1,HEIGHTI-1, format!("t = {}", t).as_str());
 
         // Quit on 'q'
         if engine.is_key_pressed(KeyCode::Char('q')) {
@@ -24,8 +56,7 @@ fn main() {
         }
 
         // Update frame
-        frame += 1;
-        offset = frame % 20;
+        t += inc;
 
         // Draw
         engine.draw();
