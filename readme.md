233bf766cb25f3fa540b5703605c9a084c495775
Niklas Larsson <niklaslarsson95@gmail.com>
2020-07-12 20:02:36 +1000

    Overhaul Boundaries

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Baseball Simulator
+This is a baseball simulator of unknown seriousness, to play around with game programming in rust.
+
+## Design sketch
+I think we should start by thinking about a pitch:
+1. The pitcher chooses a pitch - a particular pitcher should have a certain set of pitches, and a comfort level/frequency for each one. Maybe a pitcher has pitch sequences? Or the sequence is generated from some primitive stored in both the pitcher and the catcher
+2. The pitcher chooses a location - this is encompassed in the same decision making as above
+3. Some sort of accuracy + power calculation - pitcher skill, pitch difficulty, stamina level
+4. The pitch is now travelling to the hitter. There needs to be some calculation of pitcher vision - the faster a pitch is the harder it is to hit, but the more similar it is to the other pitches the hitter has seen, the easier it gets to adjust to it. Similar for pitch movement, similar pitches in a row become more visible. Hitters should probably learn from repeated ABs against the same pitcher, maybe also from watching other hitters against them.
+5. Hitter decides when to swing (or not). I'd probably start with high-mid-low, out-mid-in, and a timing (based on guessed pitch speed). All right, cracking hit, above the pitch is a popup, below is a tip, off horizontally is likely a foul...
+6. If the ball is hit, we need to determine the trajectory. Probably ambitious to do this physically - look at the above factors and determine a direction and a launch angle. From this we get landing spot and time taken.
+7. Determine fielder actions. Simple for nobody on, the correct actions for multiple people on is complicated...
--- src/main.rs
+++ src/main.rs
@@ -2,74 +2,80 @@ use console_engine::pixel;
 use console_engine::Color;
 use console_engine::KeyCode;
 
+use rand::prelude::*;
+
 fn main() {
-    // Initialise screen 20x20 characters, 10 fps
-    const WIDTH: u32 = 100;
-    const HEIGHT: u32 = 50;
+    // Constants - I fear I have gone overboard with short names, but their usage in below makes it very challenging
+    // STRIKE_WIDTH is so long that the line functions become enormous.
+    
+    // Buffer size - space between screen edge and strike zone edge - Applied on either side
+    const BUFF_X: i32 = 8;
+    const BUFF_Y: i32 = 5;
 
-    const WIDTHI: i32 = (WIDTH as i32) -1;
-    const HEIGHTI: i32 = (HEIGHT as i32) -1;
+    // Strike Zone Size - float to allow aiming maths to be fuzzy
+    const STRK_WDTH: f32 = 16.0;
+    const STRK_HGHT: f32 = 10.0;
 
-    let mut engine = console_engine::ConsoleEngine::init(WIDTH,HEIGHT,30);
+    // Screen Size - Simply the strike zone with buffer either side
+    const SCR_X: i32 = STRK_WDTH as i32 + BUFF_X * 2;
+    const SCR_Y: i32 = STRK_HGHT as i32 + BUFF_Y * 2;
+    
+    // Strike zone end boundaries - Right/Bottom edges
+    const STRK_ENDX:i32 = BUFF_X  + STRK_WDTH as i32;
+    const STRK_ENDY:i32 = BUFF_Y + STRK_HGHT as i32;
 
-    let mut t: f32 = 0.0;
-    let inc = 0.05;
+    // Initialise the screen - Add 1 to sizes to avoid -1 to all uses of SCR_X/Y below
+    let mut engine = console_engine::ConsoleEngine::init(SCR_X as u32 + 1, SCR_Y as u32 + 1, 1);
 
-    let mut xs = vec![];
-    let mut ys = vec![];
+    //let mut pitches = vec![];
+
+    let mut rng = rand::thread_rng();
+
     loop {
         // Wait for next frame + capture input
         engine.wait_frame();
-
         engine.clear_screen();
 
-        //Border Lines
-        engine.line(0, 0, WIDTHI, 0, pixel::pxl('-'));
-        engine.line(0, HEIGHTI, WIDTHI, HEIGHTI, pixel::pxl('-'));
-        engine.line(0, 0, 0, HEIGHTI, pixel::pxl('|'));
-        engine.line(WIDTHI, 0, WIDTHI, HEIGHTI, pixel::pxl('|'));
-
+        //Draw The Screen Boundary
+        engine.line(0,     0,     SCR_X, 0,     pixel::pxl('='));
+        engine.line(0,     SCR_Y, SCR_X, SCR_Y, pixel::pxl('='));
+        engine.line(0,     0,     0,     SCR_Y, pixel::pxl('║'));
+        engine.line(SCR_X, 0,     SCR_X, SCR_Y, pixel::pxl('║'));
         //Corners
-        engine.set_pxl(0,0, pixel::pxl('+'));
-        engine.set_pxl(0,HEIGHTI, pixel::pxl('+'));
-        engine.set_pxl(WIDTHI,0, pixel::pxl('+'));
-        engine.set_pxl(WIDTHI,HEIGHTI, pixel::pxl('+'));
+        engine.set_pxl(0,     0,     pixel::pxl('+'));
+        engine.set_pxl(0,     SCR_Y, pixel::pxl('+'));
+        engine.set_pxl(SCR_X, 0,     pixel::pxl('+'));
+        engine.set_pxl(SCR_X, SCR_Y, pixel::pxl('+'));
+
+        //Draw The Strike Zone
+        engine.line(BUFF_X,    BUFF_Y,    STRK_ENDX, BUFF_Y,     pixel::pxl('-'));
+        engine.line(BUFF_X,    STRK_ENDY, STRK_ENDX, STRK_ENDY,  pixel::pxl('-'));
+        engine.line(BUFF_X,    BUFF_Y,    BUFF_X,    STRK_ENDY,  pixel::pxl('|'));
+        engine.line(STRK_ENDX, BUFF_Y,    STRK_ENDX, STRK_ENDY,  pixel::pxl('|'));
+        //Corners
+        engine.set_pxl(BUFF_X,    BUFF_Y,     pixel::pxl('+'));
+        engine.set_pxl(BUFF_X,    STRK_ENDY,  pixel::pxl('+'));
+        engine.set_pxl(STRK_ENDX, BUFF_Y,     pixel::pxl('+'));
+        engine.set_pxl(STRK_ENDX, STRK_ENDY,  pixel::pxl('+'));
 
         // Add a new point 
-        /* Sine Wave
-        let t_i = (t * 5.0).floor() as i32;
-        let s_i = (t.sin() * 15.0).floor() as i32;
-        let x = t_i;
-        let y = (HEIGHTI / 2) + s_i;
-        xs.push(x);
-        ys.push(y);
-        */
+        let target_x = STRK_WDTH * random::<f32>();
+        let target_y = STRK_HGHT * random::<f32>(); 
 
-        //Spiral
-        const CENTREX: i32 = WIDTHI / 2;
-        const CENTREY: i32 = HEIGHTI / 2;
+        // TODO: Add Miss chance
+        let acc_x = 0.0;
+        let acc_y = 0.0;
 
-        let s_i = (t.sin() * t * 3.0).floor() as i32;
-        let c_i = (t.cos() * t * 1.5).floor() as i32;
-        let x = CENTREX + s_i;
-        let y = CENTREY + c_i;
-        xs.push(x);
-        ys.push(y);
+        let pitch_x = (target_x + acc_x) as i32;
+        let pitch_y = (target_y + acc_y) as i32;
 
-        for i in 0..xs.len() {
-            engine.set_pxl(xs[i],ys[i],pixel::pxl('x'));
-        }
+        engine.set_pxl(pitch_x, pitch_y, pixel::pxl('o'));
 
-        // PRint t
-        engine.print(1,HEIGHTI-1, format!("t = {}", t).as_str());
 
         // Quit on 'q'
         if engine.is_key_pressed(KeyCode::Char('q')) {
             break;
         }
-
-        // Update frame
-        t += inc;
 
         // Draw
         engine.draw();
