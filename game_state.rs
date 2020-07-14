31dd9d8893a7bc542be0f7e6f2d2e3098d727a62
Niklas Larsson <niklaslarsson95@gmail.com>
2020-07-14 18:31:23 +1000

    Introduce Game State object

---
+++ game_state.rs
@@ -0,0 +1,16 @@
+
+// Everything Should Live Here, its probably going to be horrible
+struct GameState {
+    // Home Team
+    // Away Team
+    home_score: u32,
+    away_score: u32,
+    inning: u32,
+    is_top: bool,
+    outs: u8,
+    strikes: u8,
+    balls: u8,
+    // Man on 1st
+    // Man on 2nd
+    // Man on 3rd
+}
